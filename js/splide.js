b76539ea41a852bdcb1cc3f12ef56076c9373ca1
DoboVlad <66554619+DoboVlad@users.noreply.github.com>
2021-02-09 19:13:18 +0200

    Added the new sliders on home page

--- js/splide.js
+++ js/splide.js
@@ -1,5 +1,7 @@
 var slideIndex = 1;
+var slideIndex2 = 1;
 showSlides(slideIndex);
+showSlides2(slideIndex2);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -9,6 +11,15 @@ function plusSlides(n) {
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
+}
+
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
 }
 
 function showSlides(n) {
@@ -27,4 +38,23 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides--2");
+  var dots = document.getElementsByClassName("demo--2");
+  var captionText = document.getElementById("caption--2");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2-1].style.display = "block";
+  dots[slideIndex2-1].className += " active";
+  captionText.innerHTML = dots[slideIndex2-1].alt;
 }
